7549006c998c56a992e44bba12be40ef8dd3badb
Pietro Di Bello <pietro.dibello@xpeppers.com>
2014-05-17 17:57:22 +0200

    fixing random dice roller

--- src/main/java/com/xpeppers/kata/RandomDiceRoller.java
+++ src/main/java/com/xpeppers/kata/RandomDiceRoller.java
@@ -3,13 +3,16 @@ package com.xpeppers.kata;
 import java.util.Date;
 import java.util.Random;
 
-
 public class RandomDiceRoller implements DiceRoller {
+
+    private Random random;
+
+    public RandomDiceRoller() {
+        random = new Random(new Date().getTime());
+    }
 
     @Override
     public Roll roll() {
-        Random random = new Random(new Date().getTime());
-        return Roll.dice(random.nextInt(5)+1, random.nextInt(5)+1);
+        return Roll.dice(random.nextInt(6) + 1, random.nextInt(6) + 1);
     }
-
 }
